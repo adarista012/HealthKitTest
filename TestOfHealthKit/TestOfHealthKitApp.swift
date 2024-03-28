75a73e2adc3fb792d631dac2da0d7c76d8198733
adarista012 <ad.arista012@gmail.com>
2024-03-28 18:58:29 -0400

    Initial Commit

---
+++ TestOfHealthKit/TestOfHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOfHealthKitApp.swift
+//  TestOfHealthKit
+//
+//  Created by antonio dante arista rivas on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestOfHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
